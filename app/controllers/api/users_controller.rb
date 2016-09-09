0be27ab161da2a8bff50ec2b7fd00172881b25f4
Jocelyn Hsu <jocelyn@Jocelyns-iMac.local>
2016-09-09 16:01:51 -0700

    Destroy Users and Lists added to controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,6 +16,16 @@ class Api::UsersController < ApiController
     end
   end
 
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+
   private
 
   def user_params
